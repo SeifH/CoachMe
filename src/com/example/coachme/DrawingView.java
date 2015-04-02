b652c312fe0e5982eac54b1f48ca7d779eab32e5
Olivia <Olivia@10.178.146.50>
2015-04-02 08:40:59 -0400

    Second commit

---
+++ src/com/example/coachme/DrawingView.java
@@ -0,0 +1,127 @@
+package com.example.coachme;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff;
+import android.view.MotionEvent;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawingView extends View {
+	
+	//drawing path
+	private Path drawPath;
+	//drawing and canvas paint
+	private Paint drawPaint, canvasPaint;
+	//initial color
+	private int paintColor = 0xFF660000;
+	//canvas
+	private Canvas drawCanvas;
+	//canvas bitmap
+	private Bitmap canvasBitmap;
+	private boolean erase=false;
+	
+	public void setErase(boolean isErase){
+		//set erase true or false  
+		erase = isErase;
+		if(erase) drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+		else drawPaint.setXfermode(null);
+		
+		if (isErase==true){
+			drawPaint.setStrokeWidth(50);
+
+		}else
+			drawPaint.setStrokeWidth(20);
+		}
+	
+	public DrawingView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setupDrawing();
+		
+		
+		
+	}
+	
+	private void setupDrawing()
+	{
+		//get drawing area setup for interaction
+		
+		drawPath = new Path();
+		drawPaint = new Paint();
+		
+		//set paint colour
+		drawPaint.setColor(paintColor);
+		//setproperties
+		drawPaint.setAntiAlias(true);
+		drawPaint.setStrokeWidth(20);
+		drawPaint.setStyle(Paint.Style.STROKE);
+		drawPaint.setStrokeJoin(Paint.Join.ROUND);
+		drawPaint.setStrokeCap(Paint.Cap.ROUND);
+		
+		//set up drawing
+		canvasPaint = new Paint(Paint.DITHER_FLAG);
+		
+		
+		
+	}
+	
+	public void clearCanvas()
+	{
+		drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+	    invalidate();
+	}
+	
+	//make custom view function as a drawing view
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh)
+	{
+		//view given size
+		super.onSizeChanged(w, h, oldw, oldh);
+		
+		canvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+		drawCanvas = new Canvas(canvasBitmap);
+	}
+	
+	//allowclass to function as a custom drawing view
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+		canvas.drawPath(drawPath, drawPaint);
+	}
+	
+	//facilitate drawing
+	//get user touches to create drawing
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		//detect user touch    
+		float touchX = event.getX();
+		float touchY = event.getY();
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+		    drawPath.moveTo(touchX, touchY);
+		    break;
+		case MotionEvent.ACTION_MOVE:
+		    drawPath.lineTo(touchX, touchY);
+		    break;
+		case MotionEvent.ACTION_UP:
+		    drawCanvas.drawPath(drawPath, drawPaint);
+		    drawPath.reset();
+		    break;
+		default:
+		    return false;
+		    
+		}
+		
+		//invalidate to execute onDraw
+		invalidate();
+		return true;
+		
+		
+	}
+
+}
--- src/com/example/coachme/FormationsActivity.java
+++ src/com/example/coachme/FormationsActivity.java
@@ -1,14 +1,32 @@
 package com.example.coachme;
 
 import android.app.Activity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
-public class FormationsActivity extends Activity{
+import android.content.ClipData;
+import android.graphics.Typeface;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
 
 
+public class FormationsActivity extends Activity implements OnClickListener{
+
+	private DrawingView drawView;
+	private ImageButton draw, eraser, refresh;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +37,56 @@ public class FormationsActivity extends Activity{
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.formations);
+		
+		drawView = (DrawingView)findViewById(R.id.drawing);
+		
+		eraser = (ImageButton)findViewById(R.id.erase);
+		eraser.setOnClickListener(this);
+		
+		draw = (ImageButton)findViewById(R.id.draw);
+		draw.setOnClickListener(this);
+		
+		refresh = (ImageButton)findViewById(R.id.refresh);
+		refresh.setOnClickListener(this);
 	}
+	
+	@Override
+	public void onClick(View view)
+	{
+		//what to do when clicked
+		if(view.getId()==R.id.draw){
+		    //draw button clicked
+			drawView.setErase(false); 
+		}
+		else if(view.getId()==R.id.erase){
+		    //switch to eraser
+			drawView.setErase(true); 
+
+		}
+		else if(view.getId()==R.id.refresh){
+			AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+			newDialog.setTitle("New Formation");
+			newDialog.setMessage("Start new formation? (you will lose the current progress)");
+			newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+			    public void onClick(DialogInterface dialog, int which){
+			        drawView.clearCanvas();
+			        drawView.setErase(false);
+			        dialog.dismiss();
+			    }
+			});
+			newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+			    public void onClick(DialogInterface dialog, int which){
+			        dialog.cancel();
+			    }
+			});
+			newDialog.show();
+	
+		}
+		
+		
+	
+		
+	}
+	
 	
 }
