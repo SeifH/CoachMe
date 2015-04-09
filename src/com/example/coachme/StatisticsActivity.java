4de39f4497d12ceb002fc7485d19601da64543b8
Olivia <Olivia@Gallifrey>
2015-04-09 10:03:30 -0400

    Commit4

---
+++ src/com/example/coachme/StatisticsActivity.java
@@ -0,0 +1,30 @@
+package com.example.coachme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StatisticsActivity extends Activity implements OnClickListener{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Go fullscreen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.statistics);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
